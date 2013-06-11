5ef73e0b32eef8a57960fa11882e9e02cb4d4e5a
Katharina Hayer <katharinaehayer@gmail.com>
2013-06-11 09:37:00 -0400

    hitting the pause button

--- lib/hamster.rb
+++ lib/hamster.rb
@@ -1,3 +1,6 @@
 class Hamster
 
+  def initialize()
+  end
+
 end
--- test/test_hamster.rb
+++ test/test_hamster.rb
@@ -3,6 +3,7 @@ require "hamster"
 class TestHamster < Test::Unit::TestCase
 
   def test_initialize
+    k = Hamster.new()
     assert_equal(1,1)
   end
 
